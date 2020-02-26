afa8e2a6dddec98c7cd03205ce16494fc2bea11b
Martin van Velsen <vvelsen@gmail.com>
2020-02-26 07:57:50 -0500

    Started upgrading the code and the package for future testing

--- test.sh
+++ test.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 clear
+
+echo "Clean ..."
+rm -rf ~/.m2/repository/knossys/
+cp -v ../../../../chaperone/wildfly/target/knossys-1.0.0.jar ./lib/knossys/knossys/1.0.0
+
+echo "Build ..."
 mvn package
+
+echo "Run ..."
 java -cp "./target/KThreadingTestApp-jar-with-dependencies.jar:./lib/knossys-classes.jar" com.knossys.rnd.test.KThreadTestApp
